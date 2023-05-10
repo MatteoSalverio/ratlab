230b7c5db20e7d5128e4b2c8e0d6822f5b131915
Matteo <matteosalverio134@gmail.com>
2023-05-10 09:34:31 -0400

    Rat Lab Engine Updates

--- projects/platformer/script.js
+++ projects/platformer/script.js
@@ -1,20 +1,24 @@
+// Game Setup:
 world.gravity = 0.25;
 canvas.style.backgroundColor = "darkslateblue";
 camera.offset.y = -200;
 camera.offset.x = 50;
 
+// Variables:
+var canJump = false;
+
+// Object Instantiation:
 let player = new spriteObj(500, 300, 96, 96, 10, 'assets/textures/RatRight.png', 32, 32, 4, false);
 player.hitBoxOffset = 8;
-
 let labTable = new spriteObj(700, 322, 44 * 3, 26 * 3, 30, 'assets/textures/labTable.png', 44, 26, 0, false);
 for (let i = 0; i < 3; i++) {
     for (let j = 0; j < 3; j++) {
         new box(i * 50 + 350, j * 50 + 100, 40, 40, 1, 'rgba(0, ' + i * 20 + 100 + ', ' + j * 20 + 100 + ', 1)', true);
     }
 }
-
 createBoxOfTiles(0, 400, 1200, 32, 0, 'assets/textures/blueTile.png', 32, 16);
 
+// Movement Logic:
 function processMovement(delta) {
     let speed = playerSpeed;
     if ((getKey("W") || getKey("Space")) && canJump) {// W
@@ -35,13 +39,12 @@ function processMovement(delta) {
     moveCameraToPlayer();
 }
 
-var canJump = false;
-
+// Update Function:
 function update(delta) { // Runs every frame
     if (player.colliderCheck("down"))
         canJump = true;
     else
         canJump = false;
 
-    processMovement(delta)
+    processMovement(delta);
 }
