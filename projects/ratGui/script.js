1460eb4d2c4f0f459d3b75e681afc7a93ba03512
Matteo <matteosalverio134@gmail.com>
2023-05-23 15:09:57 -0400

    Rat Lab Desktop Updates

--- projects/ratGui/script.js
+++ projects/ratGui/script.js
@@ -24,10 +24,31 @@ function updateTime() {
     time.innerHTML = newDate.getHours() + ":" + min// + " " + part;
 }
 
+function updateWindowPosition() {
+    if (mousedown) {
+        let w = document.getElementById(activeWindow);
+        let deltaPos = {
+            x: mousePos.x - oldMousePos.x,
+            y: mousePos.y - oldMousePos.y
+        };
+        let windowPosition = {
+            top: w.style.top.replace("px", "") * 1,
+            left: w.style.left.replace("px", "") * 1
+        }
+        w.style.left = windowPosition + deltaPos + "px";
+        w.style.top = windowPosition + deltaPos + "px";
+    }
+}
+
 setInterval(() => {
     updateTime();
+    updateWindowPosition();
 }, 1);
 
+var mousedown = false;
+var activeWindow = "";
+var oldMousePos = { x: 0, y: 0 };
+var mousePos = { x: 0, y: 0 };
 function openWindow(id) {
     if (document.getElementById(id) != null) {
         toggleElement(id);
@@ -35,14 +56,30 @@ function openWindow(id) {
     }
     let temp = "";
     temp += "<div class='window' id='" + id + "' style='display: inline;'>";
-    temp += '<div class="controls">';
+    temp += '<div class="controls" id="' + id + '_controls">';
     temp += '<div class="controlBtns">';
     temp += '<button class="controlsBtn closeBtn" onclick="' + "closeWindow('" + id + "')" + '">X</button>';
     temp += '<button class="controlsBtn minMaxBtn" onclick="' + "maximizeWindow('" + id + "')" + '">~</button>';
     temp += '</div></div>';
     temp += '<iframe class="windowFrame" src="apps/' + id + '.html" width="100%" height="100%"></iframe>'
     windows.innerHTML += temp;
+    let controls = document.getElementById(id + "_controls");
+    controls.addEventListener("mousedown", function (e) {
+        mousedown = true;
+        activeWindow = id;
+        controls.parentElement.style.transform = "translate(-50%, 0)";
+    });
+    controls.addEventListener("mouseup", function () {
+        mousedown = false;
+    });
 }
+
+document.addEventListener("mousemove", function (e) {
+    oldMousePos.x = mousePos.x;
+    oldMousePos.y = mousePos.y;
+    mousePos.x = e.clientX;
+    mousePos.y = e.clientY;
+});
 
 function minimizeWindow(id) {
     document.getElementById(id).style.display = 'none';
@@ -67,6 +104,17 @@ function maximizeWindow(id) {
         elem.firstChild.style.borderRadius = "0px";
         elem.lastChild.style.borderRadius = "0px";
     }
+    else {
+        elem.style.width = "70%";
+        elem.style.height = "70%";
+
+        elem.style.transform = "translate(-50%, -50%)";
+        elem.style.top = "45%";
+
+        elem.style.borderRadius = "15px";
+        elem.firstChild.style.borderRadius = "15px 15px 0px 0px";
+        elem.lastChild.style.borderRadius = "0px 0px 15px 15px";
+    }
 }
 
-openWindow("browser");
+openWindow("notes");
