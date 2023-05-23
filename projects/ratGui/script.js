2788d36714fc7bda35ada76b0bca25781c7a57e0
Matteo <matteosalverio134@gmail.com>
2023-05-23 12:04:50 -0400

    Rat Lab Desktop Updates

--- projects/ratGui/script.js
+++ projects/ratGui/script.js
@@ -1,6 +1,8 @@
 const desktop = document.getElementById("desktop");
+const taskbar = document.getElementById("taskBar");
 const date = document.getElementById("date");
 const time = document.getElementById("time");
+const windows = document.getElementById("windows");
 
 function toggleElement(id) {
     if (document.getElementById(id).style.display == "none")
@@ -25,3 +27,46 @@ function updateTime() {
 setInterval(() => {
     updateTime();
 }, 1);
+
+function openWindow(id) {
+    if (document.getElementById(id) != null) {
+        toggleElement(id);
+        return;
+    }
+    let temp = "";
+    temp += "<div class='window' id='" + id + "' style='display: inline;'>";
+    temp += '<div class="controls">';
+    temp += '<div class="controlBtns">';
+    temp += '<button class="controlsBtn closeBtn" onclick="' + "closeWindow('" + id + "')" + '">X</button>';
+    temp += '<button class="controlsBtn minMaxBtn" onclick="' + "maximizeWindow('" + id + "')" + '">~</button>';
+    temp += '</div></div>';
+    temp += '<iframe class="windowFrame" src="apps/' + id + '.html" width="100%" height="100%"></iframe>'
+    windows.innerHTML += temp;
+}
+
+function minimizeWindow(id) {
+    document.getElementById(id).style.display = 'none';
+}
+
+function closeWindow(id) {
+    windows.removeChild(document.getElementById(id))
+}
+
+function maximizeWindow(id) {
+    let elem = document.getElementById(id);
+    if (elem.style.width != "100%") {
+        elem.style.margin = "0px";
+
+        elem.style.width = "100%";
+        elem.style.height = (desktop.clientHeight - taskbar.clientHeight - 30) + "px";
+
+        elem.style.transform = "translate(-50%, 0px)";
+        elem.style.top = "0px";
+
+        elem.style.borderRadius = "0px";
+        elem.firstChild.style.borderRadius = "0px";
+        elem.lastChild.style.borderRadius = "0px";
+    }
+}
+
+openWindow("browser");
